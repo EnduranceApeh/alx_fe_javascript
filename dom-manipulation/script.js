f2a3dc8d6b4ad56052c706d9453e5d016980835c
EnduranceApeh <apehendurance2@gmail.com>
2025-12-11 13:37:46 +0100

    Initial commit

---
+++ dom-manipulation/script.js
@@ -0,0 +1,31 @@
+const quotesArray = [
+  {
+    text: "Success is the sum of small efforts repeated day in and day out",
+    category: "Motivation"
+  },
+  {
+    text: "Your mind is your greatest power—train it well.",
+    category: "Mindset"
+  }, 
+  {
+    text: "Become 1% better every single day.",
+    category: "Self-improvement"
+  }
+]
+
+const showQuoteButton = document.getElementById('newQuote')
+
+// Function to show random quote
+function showRandomQuote() {
+  const displayQuote = document.getElementById('quoteDisplay');
+ const randomQuote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
+
+  displayQuote.textContent = randomQuote.text;
+}
+
+
+function createAddQuoteForm() {
+  
+}
+
+showQuoteButton.addEventListener('click', showRandomQuote)
