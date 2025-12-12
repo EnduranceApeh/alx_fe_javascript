d1c95af3a867e72c2d22eedbae59e7e30c30e0e4
EnduranceApeh <apehendurance2@gmail.com>
2025-12-12 18:26:39 +0100

    Implemented Web Storage and JSON Handling

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,24 +1,12 @@
-const quotesArray = [
-  {
-    text: "Success is the sum of small efforts repeated day in and day out",
-    category: "Motivation"
-  },
-  {
-    text: "Your mind is your greatest power—train it well.",
-    category: "Mindset"
-  }, 
-  {
-    text: "Become 1% better every single day.",
-    category: "Self-improvement"
-  }
-]
+const quotesArray = JSON.parse(localStorage.getItem('quotes')) || [];
 
 const showQuoteButton = document.getElementById('newQuote');
 const displayQuote = document.getElementById('quoteDisplay');
 
+
 // Function to show random quote
 function showRandomQuote() {
- const randomQuote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
+  const randomQuote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
 
   displayQuote.innerHTML = randomQuote.text;
 }
@@ -39,6 +27,9 @@ function addQuote() {
     }
   );
 
+  localStorage.setItem('quotes', JSON.stringify(quotesArray))
+
+
   //clean input field after add button is clicked
   document.getElementById('newQuoteText').value = '';
   document.getElementById('newQuoteCategory').value = '';
@@ -53,7 +44,7 @@ function createAddQuoteForm() {
   textInput.placeholder = 'ENtera new quote';
 
   // Create input for category
-  const  categoryInput = document.createElement('input');
+  const categoryInput = document.createElement('input');
   categoryInput.id = 'newQuoteCategory';
   categoryInput.placeholder = 'Enter quote category';
 
@@ -74,3 +65,36 @@ function createAddQuoteForm() {
 }
 // Add click event to the show new quote button
 showQuoteButton.addEventListener('click', showRandomQuote);
+
+// Export quote
+
+function exportQuotes () {
+  const jsonData = JSON.stringify(quotesArray, null, 2);
+  const blob = new Blob([jsonData], {type: "application/json"});
+
+  const url = URL.createObjectURL(blob);
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'quotes.json';
+  a.click();
+
+  URL.revokeObjectURL(url)
+}
+
+// Add event listner to the export button
+
+const exportButton = document.getElementById('export-btn');
+exportButton.addEventListener('click', exportQuotes)
+
+ function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    console.log(fileReader)
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotesArray.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
